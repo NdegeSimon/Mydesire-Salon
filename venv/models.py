6bf5a0c51f27493817ee3d03a44c6fa8fa522112
Simon Ndege <simopetley@gmail.com>
2025-09-02 22:39:28 +0300

    add changes

--- venv/models.py
+++ venv/models.py
@@ -16,6 +16,12 @@ class User(Base):
     notifications = relationship("Notification", back_populates="user", cascade="all, delete")
     payments = relationship("payments", back_populates="user", cascade="all, delete")
     
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 
 class Appointment(Base):
